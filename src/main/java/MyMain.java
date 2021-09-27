d3099c7ff257577ab0f3446dde7058e0a4364c1a
anaypant <35899820+anaypant@users.noreply.github.com>
2021-09-27 12:10:02 -0700

    c

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -3,7 +3,7 @@ public class MyMain {
     // Returns a random integer between 1 and 6
     public static int rollDie() {
         // REPLACE YOUR CODE HERE
-        return -1;
+        return (int) (Math.random() * 6) + 1;
     }
 
     // Calculate the probability of rolling at least one 6 when rolling 
@@ -15,28 +15,63 @@ public class MyMain {
 
         // Your code should roll 6 dice 10,000 times, so you should have a
         // for loop such as:
+        double totalRolls = 0;
+        double sixRolls = 0;
         for (int i = 0; i < 10000; i++) {
-
+            for(int j=0;j<6;j++){
+                totalRolls ++;
+                if(rollDie() == 6){
+                    sixRolls ++;
+                }
+            }
         }
 
-        return -1.0;
+        return (sixRolls /  totalRolls) * 400;
     }
 
     // Calculate the probability of rolling at least two 6's when rolling 
     // twelve dice. Uses 10000 trials.
     public static double probabilityTwoSixes() {
         // REPLACE WITH YOUR CODE HERE
-        return -1.0;
+        double totalRolls = 12 * 10000;
+        double sixRolls = 0;
+        for (int i = 0; i < 10000; i++) {
+            double individualSixRolls = 0;
+            for(int j=0;j<12;j++){
+                if(rollDie() == 6){
+                    individualSixRolls ++;
+                }
+            }
+            if (individualSixRolls >= 2){
+                sixRolls ++;
+            }
+        }
+        return (sixRolls /  totalRolls) * 1200;
+
     }
 
     // Calculate the probability of rolling at least three 6's when rolling
     // eighteen dice. Uses 10000 trials.
     public static double probabilityThreeSixes() {
         // REPLACE WITH YOUR CODE HERE
-        return -1.0;
+        double totalRolls = 12 * 10000;
+        double sixRolls = 0;
+        for (int i = 0; i < 10000; i++) {
+            double individualSixRolls = 0;
+            for(int j=0;j<18;j++){
+                if(rollDie() == 6){
+                    individualSixRolls ++;
+                }
+            }
+            if (individualSixRolls >= 3){
+                sixRolls ++;
+            }
+        }
+        return (sixRolls /  totalRolls) * 1200;
     }
 
     public static void main(String[] args) {
         // YOUR CODE HERE
+        System.out.println(probabilityTwoSixes());
     }
 }
